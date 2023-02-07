1915cd47a4c42d4f97cf20869f9033299b048abf
desktop yoochanhong <chanhongy6@naver.com>
2023-02-08 02:06:30 +0900

    jump 함수 만들기

--- flappy_bird/lib/screen/my_home_page.dart
+++ flappy_bird/lib/screen/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -8,37 +9,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double birdY = 0;
+  static double birdY = 0;
+  double initialPos = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9;
+  double velocity = 3.5;
+
+  void jump() {
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      height = gravity * time * time + velocity * time;
+      setState(() {
+        birdY = initialPos - height;
+      });
+      print(birdY);
+      if (birdY  < -1) timer.cancel();
+      time += 0.01;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.blue,
-              child: Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      alignment: Alignment(0, birdY),
-                      child: Container(
-                        width: 50,
-                        height: 50,
-                        color: Colors.yellow,
-                      ),
-                    )
-                  ],
+    return GestureDetector(
+      onTap: jump,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        alignment: Alignment(0, birdY),
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.yellow,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Container(color: Colors.brown),
-          ),
-        ],
+            Expanded(
+              child: Container(color: Colors.brown),
+            ),
+          ],
+        ),
       ),
     );
   }
