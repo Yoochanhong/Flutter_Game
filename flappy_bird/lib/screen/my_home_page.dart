1239658795a94a5d3547f1a959b5936691047ece
desktop yoochanhong <chanhongy6@naver.com>
2023-02-08 01:48:13 +0900

    캐릭터 위치 조절

--- flappy_bird/lib/screen/my_home_page.dart
+++ flappy_bird/lib/screen/my_home_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double birdY = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +17,23 @@ class MyHomePage extends StatelessWidget {
         children: [
           Expanded(
             flex: 3,
-            child: Container(color: Colors.blue),
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment(0, birdY),
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.yellow,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
           Expanded(
             child: Container(color: Colors.brown),
